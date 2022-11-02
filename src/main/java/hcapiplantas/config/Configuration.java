d989fcde96df45f14ff86d916fe1098143d48619
dev-amandacsm <dev.amandacsm@gmail.com>
2022-11-02 14:54:47 -0300

    add bean ModelMapper
    Dependência que será usada para mapear objetos DTO para Entidades e vice versa

---
+++ src/main/java/hcapiplantas/config/Configuration.java
@@ -0,0 +1,14 @@
+package hcapiplantas.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
